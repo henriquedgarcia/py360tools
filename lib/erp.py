9e19d7beb73d63b95e7bec9df19a6a56bd0c66ec
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-05-10 20:07:35 -0300

    Update erp.py

--- lib/erp.py
+++ lib/erp.py
@@ -1,56 +1,34 @@
 from typing import Union
 
 import numpy as np
+
+from .projectionbase import ProjBase, ea2xyz, xyz2ea, normalize_ea
+
+
 # from PIL import Image
 
-from .projectionbase import ProjBase, ea2xyz, xyz2ea
+
 # from .util import compose
 
 
 class ERP(ProjBase):
-    def nm2xyz(self, nm: np.ndarray,
+    def nm2xyz(self,
+               nm: np.ndarray,
                proj_shape: Union[np.ndarray, tuple]
                ) -> np.ndarray:
         if proj_shape is None:
             proj_shape = nm.shape[1:]
 
-        vu = self.erp2vu(nm, proj_shape=proj_shape)
-        ea = self.vu2ea(vu)
-        xyz = ea2xyz(ea)  # common
+        vu = erp2vu(nm=nm,
+                    proj_shape=proj_shape)
+        ea = vu2ea(vu=vu)
+        xyz = ea2xyz(ea=ea)  # common
         return xyz
 
-    @staticmethod
-    def erp2vu(nm: np.ndarray, proj_shape=None) -> np.ndarray:
-        if proj_shape is None:
-            proj_shape = nm.shape[1:]
-
-        shape = [2]
-        for i in range(len(nm.shape) - 1):
-            shape.append(1)
-
-        n1 = np.asarray([0.5, 0.5]).reshape(shape)
-        n2 = np.asarray([proj_shape[0], proj_shape[1]]).reshape(shape)
-
-        vu = (nm + n1) / n2
-        return vu
-
-    @staticmethod
-    def vu2ea(vu: np.ndarray) -> np.ndarray:
-
-        shape = [2]
-        for i in range(len(vu.shape) - 1):
-            shape.append(1)
-
-        n1 = np.asarray([-np.pi, 2 * np.pi]).reshape(shape)
-        n2 = np.asarray([np.pi / 2, -np.pi]).reshape(shape)
-
-        ea = (vu * n1) + n2
-        return ea
-
-    ###########################################################
-
-    def xyz2nm(self, xyz: np.ndarray,
-               proj_shape: np.ndarray = None):
+    def xyz2nm(self,
+               xyz: np.ndarray,
+               proj_shape: np.ndarray = None
+               ) -> np.ndarray:
         """
         ERP specific.
 
@@ -61,42 +39,104 @@ class ERP(ProjBase):
         if proj_shape is None:
             proj_shape = xyz.shape[:2]
 
-        ea = xyz2ea(xyz)
-        vu = self.ea2vu(ea)
-        nm = self.vu2nm(vu, proj_shape)
+        ea = xyz2ea(xyz=xyz)
+        vu = ea2vu(ea=ea)
+        nm = vu2erp(vu=vu,
+                    proj_shape=proj_shape)
 
         return nm
 
-    @staticmethod
-    def ea2vu(ea):
-        """
 
-        :param ea: shape==(2,...)
-        :return:
-        """
+def erp2vu(*,
+           nm: np.ndarray,
+           proj_shape=None
+           ) -> np.ndarray:
+    if proj_shape is None:
+        proj_shape = nm.shape[1:]
 
-        vu = np.zeros(ea.shape)
-        vu[0] = -ea[0] / np.pi + 0.5
-        vu[1] = ea[1] / (2 * np.pi) + 0.5
-        return vu
+    shape = [2]
+    for i in range(len(nm.shape) - 1):
+        shape.append(1)
 
-    @staticmethod
-    def vu2nm(vu, proj_shape=None):
-        if proj_shape is None:
-            proj_shape = vu.shape[1:]
+    n1 = np.asarray([0.5, 0.5]).reshape(shape)
+    n2 = np.asarray([proj_shape[0], proj_shape[1]]).reshape(shape)
 
-        shape = [2]
-        for i in range(len(vu.shape) - 1):
-            shape.append(1)
+    vu = (nm + n1) / n2
+    return vu
 
-        n1 = np.asarray([proj_shape[0], proj_shape[1]]).reshape(shape)
 
-        nm = vu * n1
-        nm = np.floor(nm)
-        return nm.astype(int)
+def vu2ea(*,
+          vu: np.ndarray
+          ) -> np.ndarray:
+    shape = [2]
+    for i in range(len(vu.shape) - 1):
+        shape.append(1)
 
-    #############################
+    n1 = np.asarray([-np.pi, 2 * np.pi]).reshape(shape)
+    n2 = np.asarray([np.pi / 2, -np.pi]).reshape(shape)
 
+    ea = (vu * n1) + n2
+    return ea
+
+
+def ea2vu(*,
+          ea
+          ) -> np.ndarray:
+    """
+
+    :param ea: shape==(2,...)
+    :return:
+    """
+
+    vu = np.zeros(ea.shape)
+    vu[0] = -ea[0] / np.pi + 0.5
+    vu[1] = ea[1] / (2 * np.pi) + 0.5
+    return vu
+
+
+def vu2erp(*,
+           vu,
+           proj_shape=None
+           ) -> np.ndarray:
+    if proj_shape is None:
+        proj_shape = vu.shape[1:]
+
+    shape = [2]
+    for i in range(len(vu.shape) - 1):
+        shape.append(1)
+
+    n1 = np.asarray([proj_shape[0], proj_shape[1]]).reshape(shape)
+
+    nm = vu * n1
+    nm = np.floor(nm=nm)
+    return nm.astype(int)
+
+
+def ea2erp(*,
+           ea: np.ndarray,
+           proj_shape=None
+           ) -> np.ndarray:
+    """
+
+    :param ea: in rad
+    :param proj_shape: shape of projection in numpy format: (height, width)
+    :return: (m, n) pixel coord using nearest neighbor
+    """
+    ea = normalize_ea(ea=ea)
+    vu = ea2vu(ea=ea)
+    nm = vu2erp(vu=vu,
+                proj_shape=proj_shape)
+    return nm
+
+
+def erp2ea(*,
+           nm: np.ndarray,
+           proj_shape=None
+           ) -> np.ndarray:
+    vu = erp2vu(nm=nm,
+                proj_shape=proj_shape)
+    ea = vu2ea(vu=vu)
+    return ea
 
 # def test_erp():
 #     # erp '144x72', '288x144','432x216','576x288'
