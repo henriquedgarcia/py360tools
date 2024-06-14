c2fb9358777174c6cf5f1d3e381c9f0c4c6f7d68
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-14 20:48:47 -0300

    upgrade test cmp
    upgrade test cmp files

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from test.test_cmp.test_cmp import test_cmp
+from test.test_cmp.testcmp import TestCmp
 # from test.test_cmp.test_erp import test_erp
 
 if __name__ == '__main__':
     # test_erp()
-    test_cmp()
+    TestCmp()
--- test/test_cmp/testcmp.py
+++ test/test_cmp/testcmp.py
@@ -1,53 +1,54 @@
-from typing import Union
-
-from PIL import Image
-from cmp import CMP
-
+import pickle
+from pathlib import Path
+from lib.util import test
 import numpy as np
+from PIL import Image
+
+from lib.cmp import CMP, cmp2nmface, nmface2cmp_face, nmface2vuface, vuface2nmface, vuface2xyz_face, xyz2vuface, \
+    cmp2ea_face, ea2cmp_face
+from lib.util import compose
 
 
-def test_cmp():
-    # erp '144x72', '288x144','432x216','576x288'
-    # cmp '144x96', '288x192','432x288','576x384'
-    yaw_pitch_roll = np.deg2rad((70, 0, 0))
-    height, width = 384, 576
+class TestCmp:
+    def __init__(self):
+        self.test_result()
 
-    cover_red = Image.new("RGB", (width, height), (255, 0, 0))
-    cover_green = Image.new("RGB", (width, height), (0, 255, 0))
-    cover_gray = Image.new("RGB", (width, height), (200, 200, 200))
-    cover_blue = Image.new("RGB", (width, height), (0, 0, 255))
+    def test_result(self):
+        # erp '144x72', '288x144','432x216','576x288'
+        # cmp '144x96', '288x192','432x288','576x384'
+        yaw_pitch_roll = np.deg2rad((70, 0, 0))
+        height, width = 384, 576
 
-    ########################################
-    # Open Image
-    frame_img: Union[Image, list] = Image.open('images/cmp1.png')
-    frame_img = frame_img.resize((width, height))
-    frame_array = np.asarray(frame_img)
+        ########################################
+        # Open Image
+        frame_img: Image = Image.open('images/cmp1.png')
+        frame_img = frame_img.resize((width, height))
+        frame_array = np.array(frame_img)
 
-    cmp = CMP(tiling='6x4', proj_res=f'{width}x{height}', fov='110x90')
-    tiles = cmp.get_vptiles(yaw_pitch_roll)
+        cmp = CMP(tiling='6x4', proj_res=f'{width}x{height}', fov='110x90')
+        tiles = cmp.get_vptiles(yaw_pitch_roll)
 
-    viewport_array = cmp.get_viewport(frame_array, yaw_pitch_roll)
-    vp_image = Image.fromarray(viewport_array)
-    width_vp = int(np.round(height * vp_image.width / vp_image.height))
-    vp_image_resized = vp_image.resize((width_vp, height))
+        viewport_array = cmp.get_vp_image(frame_array)
+        mask_all_tiles_borders = cmp.draw_all_tiles_borders()
+        mask_vp_tiles = cmp.draw_vp_tiles()
+        mask_vp = cmp.draw_vp_mask()
+        mask_vp_borders = cmp.draw_vp_borders()
 
-    # Get masks
-    mask_all_tiles_borders = Image.fromarray(cmp.draw_all_tiles_borders())
-    mask_vp_tiles = Image.fromarray(cmp.draw_vp_tiles(yaw_pitch_roll))
-    mask_vp = Image.fromarray(cmp.draw_vp_mask(yaw_pitch_roll, lum=200))
-    mask_vp_borders = Image.fromarray(cmp.draw_vp_borders(yaw_pitch_roll))
+        frame_arr1 = compose(frame_array, mask_all_tiles_borders, color=(255, 0, 0))
+        frame_arr2 = compose(frame_arr1, mask_vp_tiles, color=(0, 255, 0))
+        frame_arr3 = compose(frame_arr2, mask_vp, color=(255,))
+        frame_arr4 = compose(frame_arr3, mask_vp_borders, color=(0, 0, 255))
+        frame_img = Image.fromarray(frame_arr4)
 
-    # Composite mask with projection
-    frame_img = Image.composite(cover_red, frame_img, mask=mask_all_tiles_borders)
-    frame_img = Image.composite(cover_green, frame_img, mask=mask_vp_tiles)
-    frame_img = Image.composite(cover_gray, frame_img, mask=mask_vp)
-    frame_img = Image.composite(cover_blue, frame_img, mask=mask_vp_borders)
+        vp_image = Image.fromarray(viewport_array)
+        width_vp = int(np.round(height * vp_image.width / vp_image.height))
+        vp_image_resized = vp_image.resize((width_vp, height))
 
-    new_im = Image.new('RGB', (width + width_vp + 2, height), (255, 255, 255))
-    new_im.paste(frame_img, (0, 0))
-    new_im.paste(vp_image_resized, (width + 2, 0))
-    new_im.show()
-    print(f'The viewport touch the tiles {tiles}.')
+        new_im = Image.new('RGB', (width + width_vp + 2, height), (255, 255, 255))
+        new_im.paste(frame_img, (0, 0))
+        new_im.paste(vp_image_resized, (width + 2, 0))
+        new_im.show()
+        print(f'The viewport touch the tiles {tiles}.')
 
 
 class TestCMP:
@@ -79,8 +80,8 @@ class TestCMP:
         test(self.teste_cmp2ea)
 
     def teste_cmp2mn_face(self):
-        nmface = cmp2nmface(self.nm_test)
-        nm, face = nmface2cmp_face(nmface)
+        nmface = cmp2nmface(nm=self.nm_test)
+        nm, face = nmface2cmp_face(nmface=nmface)
 
         msg = ''
         if not np.array_equal(self.nm_test, nm):
@@ -88,15 +89,15 @@ class TestCMP:
         if not np.array_equal(nmface, self.nmface_test):
             msg += 'Error in nmface2cmp_face()'
 
-        nm, face = nmface2cmp_face(self.nmface_test)
+        nm, face = nmface2cmp_face(nmface=self.nmface_test)
         if not np.array_equal(self.nm_test, nm):
             msg += 'Error in cmp2nmface()'
 
         assert msg == '', msg
 
     def teste_nmface2vuface(self):
-        vuface = nmface2vuface(self.nmface_test)
-        nmface = vuface2nmface(vuface)
+        vuface = nmface2vuface(nmface=self.nmface_test)
+        nmface = vuface2nmface(vuface=vuface)
 
         msg = ''
         if not np.array_equal(nmface, self.nmface_test):
@@ -104,15 +105,15 @@ class TestCMP:
         if not np.array_equal(vuface, self.vuface_test):
             msg += 'Error in nmface2vuface()'
 
-        nmface = vuface2nmface(self.vuface_test)
+        nmface = vuface2nmface(vuface=self.vuface_test)
         if not np.array_equal(nmface, self.nmface_test):
             msg += 'Error in vuface2nmface()'
 
         assert msg == '', msg
 
     def teste_vuface2xyz(self):
-        xyz, face = vuface2xyz_face(self.vuface_test)
-        vuface = xyz2vuface(xyz)
+        xyz, face = vuface2xyz_face(vuface=self.vuface_test)
+        vuface = xyz2vuface(xyz=xyz)
 
         msg = ''
         if not np.array_equal(vuface, self.vuface_test):
@@ -120,21 +121,21 @@ class TestCMP:
         if not np.array_equal(xyz, self.xyz_face_test[0]):
             msg += 'Error in vuface2xyz_face()'
 
-        vuface = xyz2vuface(self.xyz_face_test[0])
+        vuface = xyz2vuface(xyz=self.xyz_face_test[0])
         if not np.array_equal(vuface, self.vuface_test):
             msg += 'Error in xyz2vuface()'
 
         assert msg == '', msg
 
     def teste_cmp2ea(self):
-        ea, face1 = cmp2ea_face(self.nm_test)
-        nm, face2 = ea2cmp_face(ea)
+        ea, face1 = cmp2ea_face(nm=self.nm_test)
+        nm, face2 = ea2cmp_face(ea=ea)
 
         msg = ''
         if not np.array_equal(nm, self.nm_test):
             msg += 'Error in reversion'
 
-        nm, face = ea2cmp_face(self.ea_test)
+        nm, face = ea2cmp_face(ea=self.ea_test)
         if not np.array_equal(ea, self.ea_test):
             msg += 'Error in cmp2ea_face()'
         if not np.array_equal(nm, self.nm_test):
@@ -157,7 +158,7 @@ class TestCMP:
         if nmface_file.exists():
             self.nmface_test = pickle.load(nmface_file.open('rb'))
         else:
-            self.nmface_test = cmp2nmface(self.nm_test)
+            self.nmface_test = cmp2nmface(nm=self.nm_test)
             with open(nmface_file, 'wb') as f:
                 pickle.dump(self.nmface_test, f)
 
@@ -166,7 +167,7 @@ class TestCMP:
         if vuface_file.exists():
             self.vuface_test = pickle.load(vuface_file.open('rb'))
         else:
-            self.vuface_test = nmface2vuface(self.nmface_test)
+            self.vuface_test = nmface2vuface(nmface=self.nmface_test)
             with open(vuface_file, 'wb') as f:
                 pickle.dump(self.vuface_test, f)
 
@@ -175,7 +176,7 @@ class TestCMP:
         if xyz_file.exists():
             self.xyz_face_test = pickle.load(xyz_file.open('rb'))
         else:
-            self.xyz_face_test = vuface2xyz_face(self.vuface_test)
+            self.xyz_face_test = vuface2xyz_face(vuface=self.vuface_test)
             with open(xyz_file, 'wb') as f:
                 pickle.dump(self.xyz_face_test, f)
 
@@ -184,7 +185,7 @@ class TestCMP:
         if ea_file.exists():
             self.ea_test = pickle.load(ea_file.open('rb'))
         else:
-            self.ea_test, face1 = cmp2ea_face(self.nm_test)
+            self.ea_test, face1 = cmp2ea_face(nm=self.nm_test)
 
             with open(ea_file, 'wb') as f:
                 pickle.dump(self.ea_test, f)
@@ -195,8 +196,7 @@ class TestCMP:
         if ea_cmp_file.exists():
             self.ea_cmp_face_test = pickle.load(ea_cmp_file.open('rb'))
         else:
-            self.ea_cmp_face_test = ea2cmp_face(self.ea_test)
+            self.ea_cmp_face_test = ea2cmp_face(ea=self.ea_test)
 
             with open(ea_cmp_file, 'wb') as f:
                 pickle.dump(self.ea_cmp_face_test, f)
-
