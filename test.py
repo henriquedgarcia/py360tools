3176626e6c16c78f8d1cf47c632987c329175451
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-19 22:22:09 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-from test.test_cmp.testcmp import TestCmp
-# from test.test_cmp.test_erp import test_erp
+import unittest
+
+from test.test_cmp.test_cmp import TestCmp
+from test.test_erp.test_erp import TestErp
+from test.test_utils.test_hm import TestPosition2Trajectory
+
+
+def create_test_suite():
+    suite = unittest.TestSuite()
+
+    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestPosition2Trajectory))
+    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestCmp))
+    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestErp))
+
+    return suite
+
 
 if __name__ == '__main__':
-    # test_erp()
-    TestCmp()
+    test_suite = create_test_suite()
+
+    runner = unittest.TextTestRunner()
+    runner.run(test_suite)
