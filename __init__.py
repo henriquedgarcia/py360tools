5859c583cec90d27c48cea020ab121fb9e917d46
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-19 22:23:23 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,1 @@
-from .lib.util import compose
-from .lib.cmp import (CMP, cmp2nmface, nmface2vuface, vuface2xyz_face, xyz2vuface, vuface2nmface, nmface2cmp_face,
-                      xyz2cmp_face, cmp2xyz_face, ea2cmp_face, cmp2ea_face)
-from .lib.erp import ERP, erp2vu, vu2ea, ea2vu, vu2erp, ea2erp, erp2ea
-from .lib.projectionbase import ProjBase, xyz2ea, ea2xyz
+pass
