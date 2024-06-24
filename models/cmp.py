b795ad654259083e4501c8349dec98057669693e
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-24 16:40:22 -0300

    Update cmp.py

--- models/cmp.py
+++ models/cmp.py
@@ -1,23 +1,17 @@
-from models.projectionbase import ProjBase
+from models.projectionbase import ProjectionBase
 from utils.transform_cmp import cmp_cmp2nmface, cmp_nmface2vuface, cmp_vuface2xyz_face, cmp_xyz2vuface, \
     cmp_vuface2nmface, cmp_nmface2cmp_face
 
 
-class CMP(ProjBase):
-    def nm2xyz(self, nm, proj_shape=None):
-        if proj_shape is None:
-            proj_shape = nm.shape[:2]
-
-        nmface = cmp_cmp2nmface(nm=nm, proj_shape=proj_shape)
-        vuface = cmp_nmface2vuface(nmface=nmface, proj_shape=proj_shape)
+class CMP(ProjectionBase):
+    def nm2xyz(self, nm):
+        nmface = cmp_cmp2nmface(nm=nm, proj_shape=self.shape)
+        vuface = cmp_nmface2vuface(nmface=nmface, proj_shape=self.shape)
         xyz, face = cmp_vuface2xyz_face(vuface=vuface)
         return xyz
 
-    def xyz2nm(self, xyz, proj_shape=None):
-        if proj_shape is None:
-            proj_shape = xyz.shape[:2]
-
+    def xyz2nm(self, xyz):
         vuface = cmp_xyz2vuface(xyz=xyz)
-        nmface = cmp_vuface2nmface(vuface=vuface, proj_shape=proj_shape)
-        cmp, face = cmp_nmface2cmp_face(nmface=nmface, proj_shape=proj_shape)
+        nmface = cmp_vuface2nmface(vuface=vuface, proj_shape=self.shape)
+        cmp, face = cmp_nmface2cmp_face(nmface=nmface, proj_shape=self.shape)
         return cmp
