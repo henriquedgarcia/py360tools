f11d47f2831d7e25ced6758a7534097b2b395119
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-24 16:40:24 -0300

    Update erp.py

--- models/erp.py
+++ models/erp.py
@@ -1,23 +1,17 @@
-from models.projectionbase import ProjBase
+from models.projectionbase import ProjectionBase
 from utils.transform import ea2xyz, xyz2ea
 from utils.transform_erp import erp_erp2vu, erp_vu2ea, erp_ea2vu, erp_vu2erp
 
 
-class ERP(ProjBase):
-    def nm2xyz(self, nm, proj_shape=None):
-        if proj_shape is None:
-            proj_shape = nm.shape[1:]
-
-        vu = erp_erp2vu(nm=nm, proj_shape=proj_shape)
+class ERP(ProjectionBase):
+    def nm2xyz(self, nm):
+        vu = erp_erp2vu(nm=nm, proj_shape=self.shape)
         ea = erp_vu2ea(vu=vu)
         xyz = ea2xyz(ea=ea)  # common
         return xyz
 
-    def xyz2nm(self, xyz, proj_shape=None):
-        if proj_shape is None:
-            proj_shape = xyz.shape[:2]
-
+    def xyz2nm(self, xyz):
         ea = xyz2ea(xyz=xyz)
         vu = erp_ea2vu(ea=ea)
-        nm = erp_vu2erp(vu=vu, proj_shape=proj_shape)
+        nm = erp_vu2erp(vu=vu, proj_shape=self.shape)
         return nm
