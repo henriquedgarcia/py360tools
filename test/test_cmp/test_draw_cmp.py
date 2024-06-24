6e436c3c21a8772d11b538f2f36001c350ac755f
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-24 18:35:30 -0300

    Update test_draw_cmp.py

--- test/test_cmp/test_draw_cmp.py
+++ test/test_cmp/test_draw_cmp.py
@@ -6,8 +6,8 @@ import numpy as np
 from PIL import Image
 from models import CMP, Viewport
 from helper.draw import draw
-from utils.transform_cmp import cmp_cmp2nmface, cmp_nmface2vuface, cmp_vuface2xyz_face, cmp_xyz2vuface, \
-    cmp_vuface2nmface, cmp_nmface2cmp_face, cmp_ea2cmp_face, cmp_cmp2ea_face
+from models.cmp import (cmp2nmface, nmface2vuface, vuface2xyz_face, xyz2vuface, vuface2nmface, nmface2cmp_face,
+                        ea2cmp_face, cmp2ea_face)
 from utils.util import test, show
 
 show = show
@@ -15,7 +15,7 @@ __FILENAME__ = Path(__file__).absolute()
 __PATH__ = __FILENAME__.parent
 
 
-class TestDrawMethods(unittest.TestCase):
+class TestDrawMethodsCmp(unittest.TestCase):
     projection: CMP
     viewport: Viewport
     frame_array: np.ndarray
@@ -26,7 +26,7 @@ class TestDrawMethods(unittest.TestCase):
         # cmp '144x96', '288x192','432x288','576x384'
         height, width = 384, 576
 
-        cls.projection = CMP(tiling='6x4', proj_res=f'{width}x{height}', fov='110x90')
+        cls.projection = CMP(tiling='6x4', proj_res=f'{width}x{height}')
         cls.viewport = Viewport(vp_shape=np.array([500, 600]),
                                 fov=np.array([110, 90]))
         cls.viewport.yaw_pitch_roll = np.deg2rad((70, 0, 0))
@@ -146,8 +146,8 @@ class TestCmpOld:
         test(self.teste_cmp2ea)
 
     def teste_cmp2mn_face(self):
-        nmface = cmp_cmp2nmface(nm=self.nm_test)
-        nm, face = cmp_nmface2cmp_face(nmface=nmface)
+        nmface = cmp2nmface(nm=self.nm_test)
+        nm, face = nmface2cmp_face(nmface=nmface)
 
         msg = ''
         if not np.array_equal(self.nm_test, nm):
@@ -155,15 +155,15 @@ class TestCmpOld:
         if not np.array_equal(nmface, self.nmface_test):
             msg += 'Error in nmface2cmp_face()'
 
-        nm, face = cmp_nmface2cmp_face(nmface=self.nmface_test)
+        nm, face = nmface2cmp_face(nmface=self.nmface_test)
         if not np.array_equal(self.nm_test, nm):
             msg += 'Error in cmp2nmface()'
 
         assert msg == '', msg
 
     def teste_nmface2vuface(self):
-        vuface = cmp_nmface2vuface(nmface=self.nmface_test)
-        nmface = cmp_vuface2nmface(vuface=vuface)
+        vuface = nmface2vuface(nmface=self.nmface_test)
+        nmface = vuface2nmface(vuface=vuface)
 
         msg = ''
         if not np.array_equal(nmface, self.nmface_test):
@@ -171,15 +171,15 @@ class TestCmpOld:
         if not np.array_equal(vuface, self.vuface_test):
             msg += 'Error in nmface2vuface()'
 
-        nmface = cmp_vuface2nmface(vuface=self.vuface_test)
+        nmface = vuface2nmface(vuface=self.vuface_test)
         if not np.array_equal(nmface, self.nmface_test):
             msg += 'Error in vuface2nmface()'
 
         assert msg == '', msg
 
     def teste_vuface2xyz(self):
-        xyz, face = cmp_vuface2xyz_face(vuface=self.vuface_test)
-        vuface = cmp_xyz2vuface(xyz=xyz)
+        xyz, face = vuface2xyz_face(vuface=self.vuface_test)
+        vuface = xyz2vuface(xyz=xyz)
 
         msg = ''
         if not np.array_equal(vuface, self.vuface_test):
@@ -187,21 +187,21 @@ class TestCmpOld:
         if not np.array_equal(xyz, self.xyz_face_test[0]):
             msg += 'Error in vuface2xyz_face()'
 
-        vuface = cmp_xyz2vuface(xyz=self.xyz_face_test[0])
+        vuface = xyz2vuface(xyz=self.xyz_face_test[0])
         if not np.array_equal(vuface, self.vuface_test):
             msg += 'Error in xyz2vuface()'
 
         assert msg == '', msg
 
     def teste_cmp2ea(self):
-        ea, face1 = cmp_cmp2ea_face(nm=self.nm_test)
-        nm, face2 = cmp_ea2cmp_face(ea=ea)
+        ea, face1 = cmp2ea_face(nm=self.nm_test)
+        nm, face2 = ea2cmp_face(ea=ea)
 
         msg = ''
         if not np.array_equal(nm, self.nm_test):
             msg += 'Error in reversion'
 
-        nm, face = cmp_ea2cmp_face(ea=self.ea_test)
+        nm, face = ea2cmp_face(ea=self.ea_test)
         if not np.array_equal(ea, self.ea_test):
             msg += 'Error in cmp2ea_face()'
         if not np.array_equal(nm, self.nm_test):
@@ -224,7 +224,7 @@ class TestCmpOld:
         if nmface_file.exists():
             self.nmface_test = pickle.load(nmface_file.open('rb'))
         else:
-            self.nmface_test = cmp_cmp2nmface(nm=self.nm_test)
+            self.nmface_test = cmp2nmface(nm=self.nm_test)
             with open(nmface_file, 'wb') as f:
                 pickle.dump(self.nmface_test, f)
 
@@ -233,7 +233,7 @@ class TestCmpOld:
         if vuface_file.exists():
             self.vuface_test = pickle.load(vuface_file.open('rb'))
         else:
-            self.vuface_test = cmp_nmface2vuface(nmface=self.nmface_test)
+            self.vuface_test = nmface2vuface(nmface=self.nmface_test)
             with open(vuface_file, 'wb') as f:
                 pickle.dump(self.vuface_test, f)
 
@@ -242,7 +242,7 @@ class TestCmpOld:
         if xyz_file.exists():
             self.xyz_face_test = pickle.load(xyz_file.open('rb'))
         else:
-            self.xyz_face_test = cmp_vuface2xyz_face(vuface=self.vuface_test)
+            self.xyz_face_test = vuface2xyz_face(vuface=self.vuface_test)
             with open(xyz_file, 'wb') as f:
                 pickle.dump(self.xyz_face_test, f)
 
@@ -251,7 +251,7 @@ class TestCmpOld:
         if ea_file.exists():
             self.ea_test = pickle.load(ea_file.open('rb'))
         else:
-            self.ea_test, face1 = cmp_cmp2ea_face(nm=self.nm_test)
+            self.ea_test, face1 = cmp2ea_face(nm=self.nm_test)
 
             with open(ea_file, 'wb') as f:
                 pickle.dump(self.ea_test, f)
@@ -262,7 +262,7 @@ class TestCmpOld:
         if ea_cmp_file.exists():
             self.ea_cmp_face_test = pickle.load(ea_cmp_file.open('rb'))
         else:
-            self.ea_cmp_face_test = cmp_ea2cmp_face(ea=self.ea_test)
+            self.ea_cmp_face_test = ea2cmp_face(ea=self.ea_test)
 
             with open(ea_cmp_file, 'wb') as f:
                 pickle.dump(self.ea_cmp_face_test, f)
