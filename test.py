ff6835dcf78309c9cdd720c848b18701415a0b57
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-28 22:07:23 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from test.test_cmp.test_cmp import TestDrawMethods
+from test.test_cmp.test_draw_cmp import TestDrawMethodsCmp
 from test.test_erp.test_erp import TestErp
 from test.test_utils.test_hm import TestPosition2Trajectory
 
@@ -9,7 +9,7 @@ def create_test_suite():
     suite = unittest.TestSuite()
 
     suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestPosition2Trajectory))
-    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestDrawMethods))
+    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestDrawMethodsCmp))
     suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestErp))
 
     return suite
