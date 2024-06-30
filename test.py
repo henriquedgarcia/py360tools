01824521157fb38f2d8e8e21929f88f50829e339
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-30 17:21:50 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,24 @@
 import unittest
 
-from test.test_cmp.test_draw_cmp import TestDrawMethodsCmp
-from test.test_erp.test_erp import TestErp
 from test.test_utils.test_hm import TestPosition2Trajectory
+from test.test_cmp.test_cmp_transform import TestCmpTransform
+from test.test_cmp.test_cmp import TestCmp
+
+from test.test_cmp.test_draw_cmp import TestCmpDrawMethods
+from test.test_erp.test_erp import TestErp
+from test.test_viewport.test_viewport import TestViewport
 
 
 def create_test_suite():
     suite = unittest.TestSuite()
 
-    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestPosition2Trajectory))
-    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestDrawMethodsCmp))
-    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestErp))
-
+    # suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestPosition2Trajectory))
+    # suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestCmpTransform))
+    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestCmp))
+    # suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestCmpDrawMethods))
+    # suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestErp))
+    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestViewport))
+    #
     return suite
 
 
