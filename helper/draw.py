748314b63340e5a5324fe6d59d2f566b3e680801
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-30 17:21:01 -0300

    Update draw.py

--- helper/draw.py
+++ helper/draw.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from utils.util import show, get_borders_value
+from models.projectionbase import ProjectionBase
 
 
 class DrawHelper:
@@ -9,7 +10,7 @@ class DrawHelper:
         show(value)
 
     @staticmethod
-    def draw_tile_border(*, projection, idx, lum=255) -> np.ndarray:
+    def draw_tile_border(*, projection: ProjectionBase, idx, lum=255) -> np.ndarray:
         """
 
         :param projection:
@@ -18,53 +19,51 @@ class DrawHelper:
         :param lum:
         :return:
         """
-        canvas = np.zeros(projection.projection, dtype='uint8')
-        canvas[projection.get_tile_borders_nm(idx)[0], projection.get_tile_borders_nm(idx)[1]] = lum
+        canvas = np.zeros(projection.shape, dtype='uint8')
+        canvas[projection.tile_list[idx].borders[0], projection.tile_list[idx].borders[1]] = lum
         return canvas
 
     @classmethod
-    def draw_all_tiles_borders(cls, *, projection, lum=255):
+    def draw_all_tiles_borders(cls, *, projection: ProjectionBase, lum=255):
         """
 
         :param projection:
         :param lum:
         :return:
         """
-        canvas = np.zeros(projection.projection, dtype='uint8')
-        for tile in projection.tiling.tile_list:
+        canvas = np.zeros(projection.shape, dtype='uint8')
+        for tile in projection.tile_list:
             canvas = canvas + cls.draw_tile_border(projection=projection, idx=int(tile), lum=lum)
         return canvas
 
-    def draw_vp_tiles(self, *, projection, lum=255):
+    def draw_vp_tiles(self, *, projection: ProjectionBase, lum=255):
         """
 
         :param projection:
         :param lum:
         :return:
         """
-        canvas = np.zeros(projection.projection, dtype='uint8')
-        for tile in projection.get_vptiles():
+        canvas = np.zeros(projection.shape, dtype='uint8')
+        for tile in projection.vptiles:
             canvas = canvas + self.draw_tile_border(projection=projection, idx=int(tile), lum=lum)
         return canvas
 
     @staticmethod
-    def draw_vp_mask(*, projection, lum=255) -> np.ndarray:
+    def draw_vp_mask(*, projection: ProjectionBase, lum=255) -> np.ndarray:
         """
         Project the sphere using ERP. Where is Viewport the
         :param projection:
         :param lum: value to draw line
         :return: a numpy.ndarray with one deep color
         """
-        canvas = np.zeros(projection.projection, dtype='uint8')
-
-        inner_prod = np.tensordot(projection.viewport.normals_rotated.T, projection.coord_xyz, axes=1)
-        belong = np.all(inner_prod <= 0, axis=0)
+        canvas = np.zeros(projection.shape, dtype='uint8')
+        belong = projection.viewport.is_viewport(projection.coord_xyz)
         canvas[belong] = lum
 
         return canvas
 
     @staticmethod
-    def draw_vp_borders(*, projection, thickness=1, lum=255):
+    def draw_vp_borders(*, projection: ProjectionBase, thickness=1, lum=255):
         """
         Project the sphere using ERP. Where is Viewport the
         :param projection:
@@ -72,10 +71,10 @@ class DrawHelper:
         :param thickness: in pixel.
         :return: a numpy.ndarray with one deep color
         """
-        canvas = np.zeros(projection.projection, dtype='uint8')
+        canvas = np.zeros(projection.shape, dtype='uint8')
 
         vp_borders_xyz = get_borders_value(array=projection.viewport.vp_xyz_rotated, thickness=thickness)
-        nm = projection.xyz2nm(vp_borders_xyz, proj_shape=projection.projection).astype(int)
+        nm = projection.xyz2nm(vp_borders_xyz).astype(int)
         canvas[nm[0, ...], nm[1, ...]] = lum
         return canvas
 
