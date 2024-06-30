8ca4dc007d8c8d52c787e8eed2a3d9a806ad81d5
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-06-30 17:22:18 -0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
 from .autodict import AutoDict
 from .hm import position2displacement
 
-from .transform import ea2xyz, xyz2ea, rot_matrix, check_deg, normalize_ea
+from transform.transform import ea2xyz, xyz2ea, check_deg, normalize_ea
 
-__all__ = ['AutoDict', 'position2displacement', 'ea2xyz', 'xyz2ea', 'rot_matrix', 'check_deg', 'normalize_ea']
+__all__ = ['AutoDict', 'position2displacement', 'ea2xyz', 'xyz2ea', 'check_deg', 'normalize_ea']
 
