8572c4f4f3a45c5eb0377f2dd85035037732ee24
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-07-21 16:32:04 -0300

    rename helper/ to draw/

--- py360tools/helper/__init__.py
+++
@@ -1,5 +0,0 @@
-from py360tools.helper.draw import (draw_vp_borders, draw_vp_mask, draw_tile_border, draw_scatter3d, draw_vp_tiles,
-                             draw_all_tiles_borders, compose, show)
-
-__all__ = ['draw_vp_borders', 'draw_vp_mask', 'draw_tile_border', 'draw_scatter3d', 'draw_vp_tiles',
-           'draw_all_tiles_borders', 'compose', 'show']
