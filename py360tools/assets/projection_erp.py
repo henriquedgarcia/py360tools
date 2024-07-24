83775dec4cb07aceb0eb6a16d46d9e1b069222a2
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-07-24 13:43:31 -0300

    Update projection_erp.py

--- py360tools/assets/projection_erp.py
+++ py360tools/assets/projection_erp.py
@@ -1,11 +1,11 @@
 from py360tools.assets.projection_base import ProjectionBase
-from py360tools.transform.erp_transform import erp2vu, vu2ea, ea2vu, vu2erp
+from py360tools.transform.erp_transform import nm2vu, vu2ea, ea2vu, vu2nm
 from py360tools.transform.transform import ea2xyz, xyz2ea
 
 
 class ERP(ProjectionBase):
     def nm2xyz(self, nm):
-        vu = erp2vu(nm=nm, proj_shape=self.frame.shape)
+        vu = nm2vu(nm=nm, proj_shape=self.frame.shape)
         ea = vu2ea(vu=vu)
         xyz = ea2xyz(ea=ea)  # common
         return xyz
@@ -13,5 +13,5 @@ class ERP(ProjectionBase):
     def xyz2nm(self, xyz):
         ea = xyz2ea(xyz=xyz)
         vu = ea2vu(ea=ea)
-        nm = vu2erp(vu=vu, proj_shape=self.frame.shape)
+        nm = vu2nm(vu=vu, proj_shape=self.frame.shape)
         return nm
