bdc6daee1b073171b59f555ad6a1970e13e2f73c
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-07-24 13:43:28 -0300

    Update projection_cmp.py

--- py360tools/assets/projection_cmp.py
+++ py360tools/assets/projection_cmp.py
@@ -1,11 +1,11 @@
 from py360tools.assets.projection_base import ProjectionBase
-from py360tools.transform.cmp_transform import (cmp2nmface, nmface2vuface, vuface2xyz_face, xyz2vuface, vuface2nmface,
-                                                nmface2cmp_face)
+from py360tools.transform.cmp_transform import (nm2nmface, nmface2vuface, vuface2xyz_face, xyz2vuface, vuface2nmface,
+                                                nmface2nm_face)
 
 
 class CMP(ProjectionBase):
     def nm2xyz(self, nm):
-        nmface = cmp2nmface(nm=nm, proj_shape=self.frame.shape)
+        nmface = nm2nmface(nm=nm, proj_shape=self.frame.shape)
         vuface = nmface2vuface(nmface=nmface, proj_shape=self.frame.shape)
         xyz, face = vuface2xyz_face(vuface=vuface)
         return xyz
@@ -13,5 +13,5 @@ class CMP(ProjectionBase):
     def xyz2nm(self, xyz):
         vuface = xyz2vuface(xyz=xyz)
         nmface = vuface2nmface(vuface=vuface, proj_shape=self.frame.shape)
-        cmp, face = nmface2cmp_face(nmface=nmface, proj_shape=self.frame.shape)
+        cmp, face = nmface2nm_face(nmface=nmface, proj_shape=self.frame.shape)
         return cmp
