84db3953ec22a540a433fa88bc0a44e12bbd1bfd
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-07-13 16:06:07 -0300

    Update projection_cmp.py

--- py360tools/assets/projection_cmp.py
+++ py360tools/assets/projection_cmp.py
@@ -5,13 +5,13 @@ from py360tools.transform.cmp_transform import (nm2nmface, nmface2vuface, vuface
 
 class CMP(ProjectionBase):
     def nm2xyz(self, nm):
-        nmface = nm2nmface(nm=nm, proj_shape=self.canvas.shape)
-        vuface = nmface2vuface(nmface=nmface, proj_shape=self.canvas.shape)
+        nmface = nm2nmface(nm=nm, proj_shape=self.shape)
+        vuface = nmface2vuface(nmface=nmface, proj_shape=self.shape)
         xyz, face = vuface2xyz_face(vuface=vuface)
         return xyz
 
     def xyz2nm(self, xyz):
         vuface = xyz2vuface(xyz=xyz)
-        nmface = vuface2nmface(vuface=vuface, proj_shape=self.canvas.shape)
-        cmp, face = nmface2nm_face(nmface=nmface, proj_shape=self.canvas.shape)
+        nmface = vuface2nmface(vuface=vuface, proj_shape=self.shape)
+        cmp, face = nmface2nm_face(nmface=nmface, proj_shape=self.shape)
         return cmp
