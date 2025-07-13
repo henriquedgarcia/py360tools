cb6a1d8751f43f43cb562ce460e3ee79aece6bc9
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-07-13 16:06:18 -0300

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,6 @@ from tests.test_erp.test_erp_transform import TestErpTransform
 from tests.test_utils.test_hm import TestPosition2Trajectory
 from tests.test_viewport.test_viewport import TestViewport
 
-
 if __name__ == '__main__':
     runner = unittest.TextTestRunner(verbosity=2)
 
@@ -18,18 +17,17 @@ if __name__ == '__main__':
     suite_viewport.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestPosition2Trajectory))
     suite_viewport.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestViewport))
     runner.run(suite_viewport)
-    #
+
     print('\nRUNNING CMP TESTS')
     suite_cmp = unittest.TestSuite()
     suite_cmp.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestCmpTransform))
     suite_cmp.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestCmp))
     suite_cmp.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestCmpDrawMethods))
     runner.run(suite_cmp)
-    #
+
     print('\nRUNNING ERP TESTS')
     suite_erp = unittest.TestSuite()
     suite_erp.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestErpTransform))
     suite_erp.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestErp))
     suite_erp.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(TestErpDrawMethods))
     runner.run(suite_erp)
-
