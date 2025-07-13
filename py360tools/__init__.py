0df506b69aba73c0305aad6f23694e795b93fc9a
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-07-13 16:05:59 -0300

    Update __init__.py

--- py360tools/__init__.py
+++ py360tools/__init__.py
@@ -1,6 +1,6 @@
-from py360tools.assets.projection_cmp import CMP
-from py360tools.assets.viewport import Viewport
-from py360tools.assets.projection_erp import ERP
 from py360tools.assets.projection_base import ProjectionBase
+from py360tools.assets.projection_cmp import CMP
+from py360tools.assets.projection_erp import ERP
+from py360tools.assets.viewport import Viewport
+from py360tools.draw.draw import compose, show
 from py360tools.transform.transform import xyz2ea
-from py360tools.draw.draw import compose
