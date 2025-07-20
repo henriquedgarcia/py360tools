c2063853ca4b5dd56cd79030d07af53337226c74
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-07-20 12:22:04 -0300

    Update projection_base.py

--- py360tools/assets/projection_base.py
+++ py360tools/assets/projection_base.py
@@ -1,10 +1,10 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from functools import cached_property
 from typing import Union
 
 import numpy as np
 
-from py360tools.utils.lazyproperty import LazyProperty
 from py360tools.utils.util_transform import create_nm_coords, get_borders_coord_nm
 from ..utils import splitx
 
@@ -23,7 +23,7 @@ class ProjectionBase(ABC):
     def __init__(self, *, proj_res, tiling='1x1'):
         """
 
-        :param proj_res: A string representing the projection resolution. E.g. '600x3000'
+        :param proj_res: A string representing the projection resolution. E.g. '600x300'
         :type proj_res: str
         :param tiling: A string representing the tiling. E.g. '1x1' or '3x2'
         :type tiling: str
@@ -37,21 +37,23 @@ class ProjectionBase(ABC):
         self.tiling_shape = np.array(splitx(self.tiling)[::-1], dtype=int)
         self.n_tile = self.tiling_shape.prod()
         self.tile_shape = self.shape // self.tiling_shape
-        self.tile_list: dict[int, Tile] = {}
-        self.make_tiles()
 
-    def make_tiles(self):
+        self.tile_list = self.make_tile_list()
+
+    def make_tile_list(self):
+        tile_list: dict[int, Tile] = {}
         for tile in range(self.n_tile):
             tile_position_tiling = np.unravel_index(tile, self.tiling_shape)
             tile_position_nm = (tile_position_tiling * self.tile_shape).astype(int)
             tile_borders_nm = get_borders_coord_nm(tile_position_nm, self.tile_shape).astype(int)
             tile_borders_xyz = self.nm2xyz(tile_borders_nm)
-            self.tile_list.update({tile: Tile(idx=tile,
-                                              shape=self.tile_shape,
-                                              position_tiling=tile_position_tiling,
-                                              position_nm=tile_position_nm.astype(int),
-                                              borders_nm=tile_borders_nm,
-                                              borders_xyz=tile_borders_xyz)})
+            tile_list.update({tile: Tile(idx=tile,
+                                         shape=self.tile_shape,
+                                         position_tiling=tile_position_tiling,
+                                         position_nm=tile_position_nm.astype(int),
+                                         borders_nm=tile_borders_nm,
+                                         borders_xyz=tile_borders_xyz)})
+        return tile_list
 
     @abstractmethod
     def nm2xyz(self, nm) -> np.ndarray:
@@ -75,20 +77,38 @@ class ProjectionBase(ABC):
         """
         pass
 
-    @LazyProperty
+    @cached_property
     def nm(self):
         return create_nm_coords(self.shape)
 
-    _xyz = None
-
-    @property
+    @cached_property
     def xyz(self) -> np.ndarray:
-        if not self._xyz:
-            self._xyz = self.nm2xyz(self.nm)
-        return self._xyz
+        return self.nm2xyz(self.nm)
 
     def __str__(self):
         return f'{self.__class__.__name__}({self.resolution})'
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.resolution}@{self.tiling})'
+
+    def draw_tile_border(self, tile: Tile, lum=255) -> np.ndarray:
+        """
+        Draw the borders of a tile in the canvas.
+        :param tile:
+        :param lum:
+        :return:
+        """
+        canvas = np.zeros(self.shape, dtype='uint8')
+        canvas[tile.borders_nm[0], tile.borders_nm[1]] = lum
+        return canvas
+
+    def draw_all_tiles_borders(self, lum=255):
+        """
+        Draw all borders of all tiles in the canvas.
+        :param lum:
+        :return:
+        """
+        canvas = np.zeros(self.shape, dtype='uint8')
+        for tile in self.tile_list.values():
+            canvas = canvas + self.draw_tile_border(tile=tile, lum=lum)
+        return canvas
