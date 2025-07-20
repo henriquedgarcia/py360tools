04358cc5304bb53aa98a941526e42a34c7797032
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-07-20 12:22:13 -0300

    Update sph_points.py

--- py360tools/assets/sph_points.py
+++ py360tools/assets/sph_points.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 import numpy as np
 
-from py360tools.transform.cmp_transform import ea2nm_face
-from py360tools.transform.erp_transform import ea2nm
+from py360tools import ERP
+from py360tools import CMP
 
 
 class SpherePoints:
@@ -12,15 +12,15 @@ class SpherePoints:
     def __init__(self, sph_file: Path, video_shape: tuple, proj: str):
         self.sph_points_mask = np.zeros(video_shape)
         sph_file_lines = sph_file.read_text().splitlines()[1:]
-        # for each line (sample), convert to cartesian system and horizontal system
+        # for each line (sample), convert to a cartesian system and horizontal system
         for line in sph_file_lines:
             el, az = list(map(np.deg2rad, map(float, line.strip().split())))  # to rad
             ea = np.array([[az], [el]])
 
             if proj == 'erp':
-                m, n = ea2nm(ea=ea, proj_shape=video_shape)
+                m, n = ERP.ea2nm(ea=ea, proj_shape=video_shape)
             elif proj == 'cmp':
-                (m, n), face = ea2nm_face(ea=ea, proj_shape=video_shape)
+                (m, n), face = CMP.ea2nm_face(ea=ea, proj_shape=video_shape)
             else:
                 raise ValueError(f'wrong value to {proj=}')
 
