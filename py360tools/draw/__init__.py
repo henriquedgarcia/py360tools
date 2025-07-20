437b73b1fc2614fe80990cafbf4d9e0b7549bd45
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-07-20 14:13:27 -0300

    Update __init__.py

--- py360tools/draw/__init__.py
+++ py360tools/draw/__init__.py
@@ -1,2 +1,1 @@
-from py360tools.draw.draw import (draw_vp_borders, draw_vp_mask, draw_tile_border, draw_scatter3d, draw_vp_tiles,
-                                  draw_all_tiles_borders, compose, show)
+from py360tools.draw.draw import (draw_scatter3d, compose, show)
