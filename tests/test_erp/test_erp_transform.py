1148202040a51d0ae3ca29ba5d1dc466e8bc0ed3
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-07-20 12:22:37 -0300

    Update test_erp_transform.py

--- tests/test_erp/test_erp_transform.py
+++ tests/test_erp/test_erp_transform.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 import numpy as np
 
-from py360tools.transform import nm2vu, vu2ea, vu2nm, ea2vu, ea2xyz, xyz2ea
+from py360tools.assets.projection_erp import ERP
+from py360tools.transform import ea2xyz, xyz2ea
 from py360tools.utils import create_nm_coords, create_test_default
 
 __FILENAME__ = Path(__file__).absolute()
@@ -30,40 +31,40 @@ class TestErpTransform(unittest.TestCase):
         ea_file = Path(f'{__ASSETS__}/ae.pickle')
         xyz_file = Path(f'{__ASSETS__}/xyz.pickle')
 
-        # obs: sometimes vu_test and ea_test need to be rounded (8 digits is enough). 8 digits is a good precision: One
+        # Obs: sometimes vu_test and ea_test need to be rounded (8 digits is enough). 8 digits is good precision: One
         # pixel in 4K (4320x2160) resolution has 2*pi*r / 4320 = 0,00145444 rads in the equator with r=1.
         # And in pole the last pixel center has r = 2*pi*1 / 4320 / 2 = 0.000727220521664304. So the pole pixel has
         # 0.00000108 rads.
 
         cls.nm_test = create_test_default(nm_file, create_nm_coords, shape=(height, width))
-        cls.vu_test = create_test_default(vu_file, nm2vu, nm=cls.nm_test)
-        cls.ea_test = create_test_default(ea_file, vu2ea, vu=cls.vu_test)
+        cls.vu_test = create_test_default(vu_file, ERP.nm2vu, nm=cls.nm_test)
+        cls.ea_test = create_test_default(ea_file, ERP.vu2ea, vu=cls.vu_test)
         cls.xyz_test = create_test_default(xyz_file, ea2xyz, ea=cls.ea_test)
 
     def test_nm2vu(self):
         with self.subTest('Testing nm2vu.'):
-            vu = nm2vu(nm=self.nm_test)
+            vu = ERP.nm2vu(nm=self.nm_test)
             self.assertTrue(np.array_equal(self.vu_test, vu), 'Error in nm2vu()')
 
         with self.subTest('Testing vu2nm.'):
-            nm = vu2nm(vu=self.vu_test)
+            nm = ERP.vu2nm(vu=self.vu_test)
             self.assertTrue(np.array_equal(self.nm_test, nm), 'Error in vu2nm()')
 
         with self.subTest('Testing reversion.'):
-            nm = vu2nm(vu=vu)
+            nm = ERP.vu2nm(vu=vu)
             self.assertTrue(np.array_equal(self.nm_test, nm), 'Error in reversion')
 
     def teste_vu2ea(self):
         with self.subTest('Testing vu2ea.'):
-            ea = vu2ea(vu=self.vu_test)
+            ea = ERP.vu2ea(vu=self.vu_test)
             self.assertTrue(np.array_equal(self.ea_test, ea), 'Error in vu2ea()')
 
         with self.subTest('Testing ea2vu.'):
-            vu = ea2vu(ea=self.ea_test)
+            vu = ERP.ea2vu(ea=self.ea_test)
             self.assertTrue(np.array_equal(self.vu_test.round(8), vu.round(8)), 'Error in ea2vu()')
 
         with self.subTest('Testing reversion.'):
-            vu = ea2vu(ea=ea)
+            vu = ERP.ea2vu(ea=ea)
             self.assertTrue(np.array_equal(self.vu_test.round(8), vu.round(8)), 'Error in ea2vu()')
 
     def teste_ea2xyz(self):
