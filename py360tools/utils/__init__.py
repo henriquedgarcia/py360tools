2edc2fc8ffc88eb7ca40710dec291aeb16da507a
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-07-20 12:22:30 -0300

    Update __init__.py

--- py360tools/utils/__init__.py
+++ py360tools/utils/__init__.py
@@ -1,5 +1,4 @@
 from .autodict import AutoDict
-from .lazyproperty import LazyProperty
 from .util import (splitx)
 from .util_test import load_test_data, create_test_default
 from .util_transform import create_nm_coords
