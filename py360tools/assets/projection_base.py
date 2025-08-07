dfcece724dfbf8206dd52d33b013bb53ea3bff4c
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-08-06 21:07:56 -0300

    Update projection_base.py

--- py360tools/assets/projection_base.py
+++ py360tools/assets/projection_base.py
@@ -1,22 +1,40 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from functools import cached_property
+from pathlib import Path
 from typing import Union
 
 import numpy as np
 
 from ..utils import create_nm_coords, splitx
-from ..utils.util import unflatten_index, get_borders_coord_nm
+from ..utils.util import get_borders_coord_nm
 
 
 @dataclass
 class Tile:
-    idx: int
-    shape: Union[np.ndarray]
-    position_tiling: tuple
-    position_nm: np.ndarray  # start nm point
-    borders_nm: np.ndarray
-    borders_xyz: np.ndarray
+    idx: int = None
+    shape: Union[np.ndarray] = None
+    position: Union[np.ndarray, tuple[int, int]] = None
+    path: Path = None
+
+    @cached_property
+    def borders(self) -> Union[np.ndarray, tuple[int, int]]:
+        return get_borders_coord_nm(self.position, self.shape).astype(int)
+
+    def __hash__(self):
+        return hash(f'{self.idx}@{self.position}@{self.shape}')
+
+    def __str__(self):
+        return f'{self.idx}'
+
+    def __repr__(self):
+        return f'Tile(id{self.idx}-{self.position}@{self.shape})'
+
+    def __int__(self):
+        return self.idx
+
+    def __float__(self):
+        return float(self.idx)
 
 
 class ProjectionBase(ABC):
@@ -34,25 +52,21 @@ class ProjectionBase(ABC):
 
         # Build tiling
         self.tiling: str = tiling
-        self.tiling_shape = np.array(splitx(self.tiling)[::-1], dtype=int)
+        self.tiling_shape: Union[np.ndarray, tuple[int, int]] = np.array(splitx(self.tiling)[::-1], dtype=int)
         self.n_tile = self.tiling_shape.prod()
         self.tile_shape = self.shape // self.tiling_shape
 
         self.tile_list = self.make_tile_list()
 
     def make_tile_list(self):
-        tile_list: dict[int, Tile] = {}
+        tile_list: list[Tile] = []
         for tile in range(self.n_tile):
-            tile_position_tiling = unflatten_index(tile, self.tiling_shape)[::-1]
-            tile_position_nm = (tile_position_tiling * self.tile_shape).astype(int)
-            tile_borders_nm = get_borders_coord_nm(tile_position_nm, self.tile_shape).astype(int)
-            tile_borders_xyz = self.nm2xyz(tile_borders_nm)
-            tile_list.update({tile: Tile(idx=tile,
-                                         shape=self.tile_shape,
-                                         position_tiling=tile_position_tiling,
-                                         position_nm=tile_position_nm.astype(int),
-                                         borders_nm=tile_borders_nm,
-                                         borders_xyz=tile_borders_xyz)})
+            tiling_position = np.unravel_index(tile, self.tiling_shape)  # (n, m)
+            position = (tiling_position * self.tile_shape).astype(int)  # (n, m)
+            tile_list.append(Tile(idx=tile,
+                                  shape=self.tile_shape,
+                                  position=position.astype(int),
+                                  ))
         return tile_list
 
     @abstractmethod
@@ -86,7 +100,7 @@ class ProjectionBase(ABC):
         return self.nm2xyz(self.nm)
 
     def __str__(self):
-        return f'{self.__class__.__name__}({self.resolution})'
+        return f'{self.__class__.__name__.lower()}'
 
     def __repr__(self):
         return f'{self.__class__.__name__}({self.resolution}@{self.tiling})'
@@ -99,7 +113,7 @@ class ProjectionBase(ABC):
         :return:
         """
         canvas = np.zeros(self.shape, dtype='uint8')
-        canvas[tile.borders_nm[0], tile.borders_nm[1]] = lum
+        canvas[tile.borders[0], tile.borders[1]] = lum
         return canvas
 
     def draw_all_tiles_borders(self, lum=255):
@@ -109,6 +123,6 @@ class ProjectionBase(ABC):
         :return:
         """
         canvas = np.zeros(self.shape, dtype='uint8')
-        for tile in self.tile_list.values():
+        for tile in self.tile_list:
             canvas = canvas + self.draw_tile_border(tile=tile, lum=lum)
         return canvas
