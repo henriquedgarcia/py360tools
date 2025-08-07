80ead8dfd822e3f760cad54d09f8cbb4bedb81b1
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-08-06 21:11:13 -0300

    Update __init__.py

--- py360tools/__init__.py
+++ py360tools/__init__.py
@@ -2,5 +2,6 @@ from py360tools.assets.projection_base import ProjectionBase, Tile
 from py360tools.assets.projection_cmp import CMP
 from py360tools.assets.projection_erp import ERP
 from py360tools.assets.viewport import Viewport
+from py360tools.assets.tile_stitcher import TileStitcher
 from py360tools.draw.draw import compose, show
 from py360tools.transform.transform import xyz2ea
