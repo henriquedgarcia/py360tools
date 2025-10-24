856ea7656d6dfa45ab10516a6f73e24a851cf526
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-10-24 19:08:36 -0300

    Update util_test.py

--- py360tools/utils/util_test.py
+++ py360tools/utils/util_test.py
@@ -1,10 +1,13 @@
 import pickle
+from pathlib import Path
+from typing import Callable
 
 
 def load_test_data(file_name, default_data):
     """
-    if file_name exists, loads it and returns it.
-    else, run func(**kwargs), save result in file_name as pickle and return
+    If file_name exists, loads it and returns it.
+    Else, save default_data in file_name as pickle and
+    return data
 
 
     :param file_name: The pickle file name.
@@ -20,7 +23,17 @@ def load_test_data(file_name, default_data):
     return default_data
 
 
-def create_test_default(path, func, *args, **kwargs):
+def create_test_default(path: Path, func: Callable, *args, **kwargs):
+    """
+    If 'path' exists, loads it and returns it.
+    Else, run func with (*args, **kwargs) and save results in 'path' as pickle and
+    return results
+
+
+    :param path: The pickle file name.
+    :param func: The function to run if 'path' doesn't exist.
+    :return:
+    """
     try:
         data = pickle.loads(path.read_bytes())
     except FileNotFoundError:
