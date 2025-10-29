d052a7b797df5cf8cf2dbfa1568c9e6c81208d90
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2025-10-29 02:06:33 -0300

    Update test_tilestitcher.py

--- tests/test_tilestitcher/test_tilestitcher.py
+++ tests/test_tilestitcher/test_tilestitcher.py
@@ -1,27 +1,25 @@
-import pickle
 import unittest
 from pathlib import Path
 
 import numpy as np
-from PIL import Image
 
-from py360tools import Viewport, ERP, ProjectionBase
+from py360tools import Viewport, ERP, ProjectionBase, TileStitcher, Tile
 from py360tools.draw import show
 from py360tools.utils import load_test_data
 
 show = show
 __FILENAME__ = Path(__file__).absolute()
 __PATH__ = __FILENAME__.parent
-__ASSETS__ = __PATH__ / f'assets/{__FILENAME__.stem}'
+__MEDIA__ = Path(f'tests/input/12x8')  # 'tile{tile}.mp4' de 0 a 95 (tile=range(96))
+__ASSETS__ = __PATH__ / f'assets/{__FILENAME__.stem}/'
 __ASSETS__.mkdir(parents=True, exist_ok=True)
 
-mounted_frame_file = Path(f'{__ASSETS__}/mounted_frame.pickle')
+frame_erp_test_data_file = Path(f'{__ASSETS__}/frame_erp_test_data.pickle')
+frame_cmp_test_data_file = Path(f'{__ASSETS__}/frame_cmp_test_data.pickle')
 
 
 class TestTileStitcher(unittest.TestCase):
     projection: ProjectionBase
-    frame_array: np.ndarray
-    vptiles_test_data: list
 
     @classmethod
     def setUpClass(cls):
@@ -31,53 +29,57 @@ class TestTileStitcher(unittest.TestCase):
         cls.erp = ERP(tiling='12x8', proj_res='576x288')
         cls.cmp = ERP(tiling='12x8', proj_res='576x384')
 
-        cls.projection.yaw_pitch_roll = np.deg2rad((0, 0, 0))
+        cls.yaw_pitch_roll = np.deg2rad((90, 0, 0))
+        cls.viewport_erp = Viewport('440x360', '110x90', cls.erp)
+        cls.viewport_cmp = Viewport('440x360', '110x90', cls.cmp)
 
-        frame_img: Image = Image.open('images/erp1.png')
-        frame_img = frame_img.resize((width, height))
-        cls.frame_array = np.array(frame_img)
+    def test_tile_stitcher_cmp(self):
+        # inicializar
+        tiles_seen_cmp: list[Tile]
+        tiles_seen_cmp = self.viewport_cmp.get_vptiles(self.yaw_pitch_roll)
+        for tile in tiles_seen_cmp:
+            tile.path = str(__MEDIA__ / f'erp/tile{tile.idx}.mp4')
+        tile_stitcher_cmp = TileStitcher(tiles_seen_cmp, self.cmp, gray=True)
 
-        # Load expected values
-        cls.xyz_test_data = load_test_data(xyz_file, cls.projection.xyz)
-        cls.vp_img_test_data = load_test_data(vp_img_file, cls.viewport.extract_viewport(cls.frame_array))
-        cls.vptiles_test_data = load_test_data(vptiles_file, [tile.idx for tile in cls.viewport.get_vptiles()])
+        # criar iterador
+        tile_stitcher_cmp_iter = iter(tile_stitcher_cmp)
 
-    def test_nm2xyz(self):
-        xyz = self.projection.nm2xyz(self.projection.nm)
-        self.assertTrue(np.array_equal(xyz, self.xyz_test_data))
+        # testar next
+        frame_cmp1 = next(tile_stitcher_cmp_iter)
+        self.frame_cmp_test_data = load_test_data(frame_cmp_test_data_file, frame_cmp1)
+        self.assertTrue(np.array_equal(frame_cmp1, self.frame_cmp_test_data))
+        frame_cmp2 = next(tile_stitcher_cmp_iter)
+        self.assertFalse(np.array_equal(frame_cmp2, self.frame_cmp_test_data))
 
-    def test_xyz2nm(self):
-        nm = self.projection.xyz2nm(self.xyz_test_data)
-        self.assertTrue(np.array_equal(nm, self.projection.nm))
+        # testar reset
+        tile_stitcher_cmp.reset()
+        frame_cmp1 = next(tile_stitcher_cmp_iter)
+        self.assertTrue(np.array_equal(frame_cmp1, self.frame_cmp_test_data))
+        frame_cmp2 = next(tile_stitcher_cmp_iter)
+        self.assertFalse(np.array_equal(frame_cmp2, self.frame_cmp_test_data))
 
-    def test_extract_viewport(self):
-        vp_img = self.viewport.extract_viewport(self.frame_array)
-        # show(vp_img)
-        self.assertTrue(np.array_equal(vp_img, self.vp_img_test_data))
+    def test_tile_stitcher_erp(self):
+        # inicializar
+        tiles_seen_erp: list[Tile]
+        tiles_seen_erp = self.viewport_erp.get_vptiles(self.yaw_pitch_roll)
+        for tile in tiles_seen_erp:
+            tile.path = str(__MEDIA__ / f'erp/tile{tile.idx}.mp4')
+        tile_stitcher_erp = TileStitcher(tiles_seen_erp, self.erp, gray=True)
 
-    def test_get_vptiles(self):
-        self.assertTrue(np.array_equal(self.vptiles_test_data, [tile.idx for tile in self.viewport.get_vptiles()]))
+        # criar iterador
+        tile_stitcher_erp_iter = iter(tile_stitcher_erp)
 
+        # testar next
+        frame_erp1 = next(tile_stitcher_erp_iter)
+        self.frame_erp_test_data = load_test_data(frame_erp_test_data_file, frame_erp1)
+        self.assertTrue(np.array_equal(frame_erp1, self.frame_erp_test_data))
+        frame_erp2 = next(tile_stitcher_erp_iter)
+        self.assertFalse(np.array_equal(frame_erp1, frame_erp2))
+        # show(frame_erp1)
 
-def load_xyz_test_data(xyz_test_data):
-    try:
-        xyz_test_data = pickle.loads(xyz_file.read_bytes())
-    except FileNotFoundError:
-        xyz_file.write_bytes(pickle.dumps(xyz_test_data))
-    return xyz_test_data
-
-
-def load_vp_img_test_data(vp_img):
-    try:
-        vp_img = pickle.loads(vp_img_file.read_bytes())
-    except FileNotFoundError:
-        vp_img_file.write_bytes(pickle.dumps(vp_img))
-    return vp_img
-
-
-def load_vptiles_test_data(vptiles):
-    try:
-        vptiles = pickle.loads(vptiles_file.read_bytes())
-    except FileNotFoundError:
-        vptiles_file.write_bytes(pickle.dumps(vptiles))
-    return vptiles
+        # testar reset
+        tile_stitcher_erp.reset()
+        frame_erp1 = next(tile_stitcher_erp_iter)
+        self.assertTrue(np.array_equal(frame_erp1, self.frame_erp_test_data))
+        frame_erp2 = next(tile_stitcher_erp_iter)
+        self.assertFalse(np.array_equal(frame_erp1, frame_erp2))
